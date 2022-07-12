d4e9d2954556e5953653b9b3ec375e47bc40857e
jcperkins <jcperkins12@gmail.com>
2022-07-12 16:55:31 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Follow along code for the opensesame python machine learning course",
+    author="Cory Perkins",
+    license="MIT",
+)
